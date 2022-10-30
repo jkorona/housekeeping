495de1eade32cb0a2bf1ac3a90f5c6b7f6df2bf6
Jan Korona <jan@withlocals.com>
2022-10-30 22:45:57 +0100

    prototype agenda

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,21 +1,53 @@
-import { h } from 'preact';
 import { addDoc, collection } from "firebase/firestore/lite";
-import style from './style.css';
 import { db } from 'utils/firebase';
+import style from './style.css';
+
+/*
+  type Agenda = { [person: string]: Duty[] }
+
+  type Duty = {
+    description: string;
+    date: Date;
+    status: 'done' | 'pending' | 'partly_done';
+  }
+*/
+
 
 const Home = () => {
-  const handleClick = () => {
-    addDoc(collection(db, "cities"), {
-      name: "Tokyo",
-      country: "Japan"
-    });
-  };
+  const date = new Date();
+  const agenda = {
+    maurycy: [
+      { description: 'clean the kitchen', date: new Date(), status: 'done' },
+    ],
+    leon: [
+      { description: 'clean the kitchen', date: new Date(), status: 'done' },
+    ],
+    felek: [
+      { description: 'clean the kitchen', date: new Date(), status: 'done' },
+    ],
+    albert: [
+      { description: 'clean the kitchen', date: new Date(), status: 'done' },
+    ]
+  }
 
   return (
     <div class={style.home}>
-      <h1>Home</h1>
-      <p>This is the Home component.</p>
-      <button onClick={handleClick}>Save</button>
+      <h2>{date.toLocaleDateString()}</h2>
+      <ul>
+        {Object.entries(agenda).map(([person, duties]) => (
+          <li key={person}>
+            <h3 className={style.personName}>{person}</h3>
+            <ul>
+              {duties.map((duty, index) => (
+                <li key={index}>
+                  <input type="checkbox" checked={duty.status === 'done'} />
+                  <span>{duty.description}</span>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 };
