fa3f01ab74dd8f905949f5716a647c8bb13ff007
Jan Korona <jan.korona@gmail.com>
2025-02-15 15:30:30 +0100

    fix build

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,13 @@ import { AuthError } from "next-auth";
 import { redirect } from "next/navigation";
 
 type LoginPageProps = {
-  searchParams: {
+  searchParams: Promise<{
     callbackUrl?: string;
-  };
-}
+  }>;
+};
 
-export default function LoginPage({ searchParams }: LoginPageProps) {
+export default async function LoginPage({ searchParams }: LoginPageProps) {
+  const { callbackUrl } = await searchParams;
   return (
     <main>
       <h1>Login</h1>
@@ -16,21 +17,13 @@ export default function LoginPage({ searchParams }: LoginPageProps) {
         action={async () => {
           "use server";
           try {
-            await signIn('google', {
-              redirectTo: searchParams?.callbackUrl ?? "",
+            await signIn("google", {
+              redirectTo: callbackUrl ?? "",
             });
           } catch (error) {
-            // Signin can fail for a number of reasons, such as the user
-            // not existing, or the user not having the correct role.
-            // In some cases, you may want to redirect to a custom error
             if (error instanceof AuthError) {
               return redirect(`/login?error=${error.type}`);
             }
-
-            // Otherwise if a redirects happens Next.js can handle it
-            // so you can just re-thrown the error and let Next.js handle it.
-            // Docs:
-            // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
             throw error;
           }
         }}
