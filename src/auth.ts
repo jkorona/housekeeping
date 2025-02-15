3d4b6da7dac2c6186c996f06d744a48055879f31
Jan Korona <jan.korona@gmail.com>
2025-02-15 11:41:39 +0100

    auth wip

---
+++ src/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import google from "next-auth/providers/google";
+import { authConfig } from "./auth.config";
+
+export const { handlers, auth, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
