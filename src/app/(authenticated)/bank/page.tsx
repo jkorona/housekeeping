6a9bbe4e45af6e600f02dd4a7cf8f0ed95e2cef8
Jan Korona <jan.korona@gmail.com>
2025-03-14 11:50:25 +0100

    fix query

--- src/app/(authenticated)/bank/page.tsx
+++ src/app/(authenticated)/bank/page.tsx
@@ -2,31 +2,31 @@ import { db } from "@/db";
 import { transactions } from "@/db/schema/bank";
 import { members } from "@/db/schema/chores";
 import { Heading } from "@chakra-ui/react";
-import { desc } from "drizzle-orm";
+import { desc, eq, sql } from "drizzle-orm";
 
 export default async function BankPage() {
-  const latestTransactions = await db
-    .selectDistinctOn([transactions.accountId])
-    .from(transactions)
-    .orderBy(transactions.accountId, desc(transactions.createdAt));
-  const transactionToAccount = latestTransactions.reduce(
-    (acc, transaction) => ({ ...acc, [transaction.accountId]: transaction }),
-    {}
-  );
-
-  const membersList = await db.query.members
-    .findMany({
-      orderBy: members.dateOfBirth,
+  const latestTransactionsByAccount = await db
+    .selectDistinct({
+      id: members.id,
+      name: members.name,
+      total: sql`COALESCE(last_transaction.total, 0)`.as("total"),
     })
-    .execute();
+    .from(members)
+    .leftJoin(
+      db
+        .selectDistinctOn([transactions.accountId])
+        .from(transactions)
+        .orderBy(transactions.accountId, desc(transactions.createdAt))
+        .as("last_transaction"),
+      eq(members.id, sql.raw("last_transaction.account_id"))
+    );
 
   return (
     <>
       <Heading as="h1" size="3xl">
         Bank
       </Heading>
-      {JSON.stringify(membersList, null, 3)}
-      {JSON.stringify(transactionToAccount, null, 3)}
+      {JSON.stringify(latestTransactionsByAccount, null, 3)}
     </>
   );
 }
