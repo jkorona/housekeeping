6755fadf706c06cf3f3bd6b86d6bdb8d0227b1b9
Jan Korona <jan.korona@gmail.com>
2025-03-19 00:04:47 +0100

    fix types

--- src/components/ui/link-button.tsx
+++ src/components/ui/link-button.tsx
@@ -1,13 +1,8 @@
 "use client"
 
-import Link from "next/link"
-import type { HTMLChakraProps, RecipeProps } from "@chakra-ui/react"
+import Link, { LinkProps } from "next/link"
 import { createRecipeContext } from "@chakra-ui/react"
-
-export interface LinkButtonProps
-  extends HTMLChakraProps<"a", RecipeProps<"button">> {}
 
 const { withContext } = createRecipeContext({ key: "button" })
 
-// Replace "a" with your framework's link component
-export const LinkButton = withContext<HTMLAnchorElement, LinkButtonProps>(Link)
+export const LinkButton = withContext<HTMLAnchorElement, LinkProps>(Link)
