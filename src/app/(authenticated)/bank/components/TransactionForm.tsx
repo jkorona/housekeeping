a7ab2f65598b28cdd47c8721e1835277a90426b1
Jan Korona <jan.korona@gmail.com>
2025-03-24 13:39:21 +0100

    another attempt

--- src/app/(authenticated)/bank/components/TransactionForm.tsx
+++ src/app/(authenticated)/bank/components/TransactionForm.tsx
@@ -73,7 +73,7 @@ export const TransactionForm: FC<TransactionFormProps> = ({ action }) => {
                 <Fieldset.Content>
                   <Field label="Amount" required>
                     <NumberInputRoot name="amount" defaultValue="0" w="full">
-                      <NumberInputField inputMode="numeric" />
+                      <NumberInputField type="text" inputMode="numeric" />
                     </NumberInputRoot>
                   </Field>
                   <Field label="Description">
